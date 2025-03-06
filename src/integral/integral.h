c2a8e289899c5aa4e0738c08b19971a0fbcb1cbb
Federico Grenci <fede.grenci@gmail.com>
2025-03-06 12:02:03 +0100

    calculated the integrals

--- src/integral/integral.h
+++ src/integral/integral.h
@@ -1,23 +1,32 @@
-#ifndef __
+#ifndef __INTEGRAL_H__
+#define __INTEGRAL_H__
+
 #include <malloc.h>
 
-struct Vec
+typedef struct 
 {
     double* array;
     int size;
-};
+} Vec;
 
-
-struct Vec linspace(double a, double b, int n)
+void printvec( Vec v) 
 {
-    double* array = (double*) calloc( n+1, sizeof(double) );
-    struct Vec v = {array, n+1};
-    
-    double j = 0;
+    for ( int i = 0; i < v.size; i++)
+    {
+        printf("%e ", *(v.array + i));
+    }
+}
 
+
+Vec linspace(double a, double b, int n)
+{
+    double j;
+    double* array = (double*) calloc( n+1, sizeof(double) );
+    Vec v = {array, n+1};
+    
     for (int i = 0; i < v.size; i++)
     {
-        j = i / v.size;
+        j = (double)i / v.size;
 
         *(v.array + i) = a * (1-j) + b*j;
     } 
@@ -27,20 +36,20 @@ struct Vec linspace(double a, double b, int n)
 
 double intt(double a, double b, double (*f)(double), int n)
 {
-
-    struct Vec v = linspace(a, b, n);
-
-    double h = *(v.array+1) - *v.array;
+    Vec v = linspace(a, b, n);
 
     double sum = 0;
+    double h = *(v.array+1) - *v.array;
 
     for (int i = 1; i < v.size-1; i++)
     {
-        sum += f(*(v.array+i));
+        sum += (*f)(*(v.array+i));
     }
 
-    double I = h/2 * (f(a) + f(b)) + h*(sum);
+    double I = h/2 * ((*f)(a) + (*f)(b)) + h*sum;
 
 
     return I;
 }
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "integral/integral.h"
 #include <math.h>
+#include "integral/integral.h"
 
 
 double sin2(double x) { 
@@ -8,12 +8,35 @@ double sin2(double x) {
     return f*f;
 }
 
+double x2sin2(double x) {
+    double f = x*sin(2*x);
+    return f*f;
+}
+
+double e05( double x) {
+    return exp(-.5 * x);
+}
+
+double x2e05( double x) {
+    return x*x*e05(x);
+}
+
 
 int main() {
 
-    double I = intt(0, 1, sin2, 200);
+    double I;
 
-    printf("%e\n", I);
+    I = intt(0.0, 1.0, sin2, 200);
+    printf("a = %e\n", I);
+
+    I = intt(0.0, 1.0, x2sin2, 200);
+    printf("b = %e\n", I);
+
+    I = intt(0.0, 1.0, e05, 200);
+    printf("c = %e\n", 2-I);
+
+    I = intt(0.0, 1.0, x2e05, 200);
+    printf("d = %e\n", 16-I);
 
     return 0;
 }
