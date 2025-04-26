fa5a3efe4eaf0301a71b6b01cf69871355b5f496
Federico Grenci <fede.grenci@gmail.com>
2025-04-26 19:22:59 +0200

    finished updating diag

--- src/diag/diag.c
+++ src/diag/diag.c
@@ -1,17 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "diag.h"
 
-void info(char* info) {
-    printf("\x1b[94m[+] %s\x1b[0m\n", info);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+void info(char* info, ...) {
+    va_list argptr;
+    va_start(argptr, fmt);
+        
+    printf("\x1b[94m[+] ");
+    vprintf (info, argptr);
+    printf("\x1b[0m\n");
+
+    va_end(argptr);
 }
 
-void warning(char* warning) {
-    printf("\x1b[38;5;220m[*] %s\x1b[0m\n", warning);
+void warning(char* warning, ...) {
+    va_list argptr;
+    va_start(argptr, warning);
+
+    printf("\x1b[38;5;220m[*] ");
+    vprintf (warning, argptr);    
+    printf("\x1b[0m\n");
+
+    va_end(argptr);
 }
 
-void cricical(char* error) {
-    printf("\x1b[91m[!] %s\x1b[0m\n", error);
+void critical( char* error, ... ) {
+    va_list argptr;
+    va_start(argptr, error);
+
+    printf("\x1b[91m[!] ");
+    vprintf (error, argptr);
+    printf("\x1b[0m\n");
+
     printf("Exiting...\n");
+    
+    va_end(argptr);
     exit(EXIT_FAILURE);
 }
--- src/diag/diag.h
+++ src/diag/diag.h
@@ -1,8 +1,8 @@
 #ifndef __DIAG_H__
 #define __DIAG_H__
 
-void info(char* info);
-void warning(char* warning);
-void cricical(char* error);
-
+void info(char* info, ...);
+void warning(char* warning, ...);
+void critical(char* error, ...);
+ 
 #endif//__DIAG_H__
