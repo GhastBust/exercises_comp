3f03d951dba29a48db7dc64574ab125cf09711b8
Federico Grenci <fede.grenci@gmail.com>
2025-05-07 02:45:11 +0200

    added more compatibility for window

--- src/vel-ver/part_thread.c
+++ src/vel-ver/part_thread.c
@@ -2,7 +2,7 @@
 
 #include <stdbool.h>
 #include <pthread.h>
-#include <unistd.h>
+// #include <unistd.h>
 #include <stdio.h>
 
 #include "../diag/diag.h"
@@ -37,9 +37,11 @@ void wait_or_release_all( Signal* signal ) {
     signal->current_done++;
 
     if( signal->current_done == signal->threads ) {
+        info("Releasing all threads");
         signal->current_done = 0;
         pthread_cond_broadcast(&signal->cond);
     } else { 
+        warning("Waiting for other threads to finish");
         pthread_cond_wait(&signal->cond, &signal->mutex);
     }
 
@@ -93,11 +95,7 @@ void* __step_some_vernel( void* v_arg ) {
 
     ThreadArgs* arg = (ThreadArgs*) v_arg; 
 
-    // printf("thread n: %ld, try first lock\n", arg->start / arg->len );
-    // lock(arg->mutex);
-    // printf("thread n: %ld, first lock done\n", arg->start / arg->len );
     while (!*arg->stop) {
-
 
         for ( size_t i = arg->start; i < arg->start + arg->len; i++ ) {
             Particle new_p = step_vernel_vec3(arg->sym->particles+i, arg->sym, arg->dt);
@@ -107,7 +105,7 @@ void* __step_some_vernel( void* v_arg ) {
         wait_or_release_all(arg->pos_done);
     }
 
-    printf("returning thread n: %ld\n", arg->start / arg->len);
+    // printf("returning thread n: %ld\n", arg->start / arg->len);
 
     return NULL;
 }
@@ -117,54 +115,56 @@ void step_all_vernel_threads(VernelSimulation *sym, void (*init_task)(VernelSimu
 
     pthread_t   threads[THREADS];
     ThreadArgs  args   [THREADS];
-    Mutex       mutexs [THREADS];
+    // Mutex       mutexs [THREADS];
     bool        can_exe[THREADS];
     bool        stop   = false;
+    Signal      pos_done;
+
+    init_signal( &pos_done );
     
     int len = sym->n_particles / THREADS;
     
     if (len * THREADS != sym->n_particles) {critical("Number of threads does not divide evenly the particles");}
 
-    init_all(mutexs);
-    lock_all(mutexs);
+
     
     for ( int i = 0; i < THREADS; i++ ) {
 
         args[i] = (ThreadArgs){
-            .sym = sym, .dt = dt, 
-            .len = len, .start = i*len, 
-            .mutex = mutexs+i,
+            .sym = sym, 
+            .dt = dt, 
+            .len = len, 
+            .start = i*len, 
             .stop = &stop,
-            .can_exe = &can_exe[i]
+            .pos_done = &pos_done
         };
 
         pthread_create(threads+i, NULL, __step_some_vernel, args+i);
     }
 
-    unlock_all(mutexs);
+    // unlock_all(mutexs);
 
     for ( double t = dt; t < T; t += dt ) {
 
         // (*init_task)(sym);
-        // sleep(1);
         printf("Print sys\n");
 
-        lock_all(mutexs);
+        // lock_all(mutexs);
 
         // sleep(1);
         __swap_old_new(sym);
         __mirror_particles(sym);
 
-        unlock_all(mutexs);
+        // unlock_all(mutexs);
 
         if ( t > 1*dt ) {break;}
     }
 
-    lock_all(mutexs);
+    // lock_all(mutexs);
     stop = true;
 
     for ( int i = 0; i < THREADS; i++ ) {
-        unlock(mutexs+i);
+        // unlock(mutexs+i);
         pthread_join(threads[i], NULL);
     }
 }
